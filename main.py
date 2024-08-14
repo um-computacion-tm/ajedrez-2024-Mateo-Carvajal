1ea3ba58266ea84596d9aad94e2d189aabfc3a7d
Mateo-Carvajal <me.carvajal@alumno.um.edu.ar>
2024-08-13 21:39:47 -0300

    subiendo board, chess y actualizando main

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
 def working():
     return "It's working!"
+
+from chess import Chess
+
+def main():
+    chess = Chess()
+    while true:
+        play(chess)
+
+def play(chess):
+    try:
+        print(chess.show_board())
+        from_row = int(input("From row: "))
+        from_col = int(input("From col: "))
+        to_row = int(input("To row: "))
+        to_col = int(input("To col: "))
+        chess.move(
+            from_row,
+            from_col,
+            to_row,
+            to_col
+        )
+    except exception as e:
+        print("Invalid input")
+        return
+    chess.move(
+        from_row,
+        from_col,
+        to_row,
+        to_col
+    )
