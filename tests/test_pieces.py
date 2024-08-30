8397440d9cd6384f4a887c7ee8041e0907329d84
Mateo-Carvajal <me.carvajal@alumno.um.edu.ar>
2024-08-29 22:58:49 -0300

    Agregando movimiento a pawn.py y agregando tests

--- tests/test_pieces.py
+++ tests/test_pieces.py
@@ -3,16 +3,7 @@ import unittest
 from chess.pieces import Piece
 
 class TestPieces(unittest.TestCase):
-    def test_initialization(self):
-        piece = Piece("White", True)
-        self.assertEqual(piece.__color__, "White")
-        self.assertTrue(piece.is_alive())
-
-    def test_is_alive(self):
-        piece = Piece("black", True)
-        self.assertTrue(piece.is_alive())
-        piece.alive = False  
-        self.assertFalse(piece.is_alive())
+    ...
 
 if __name__ == '__main__':
     unittest.main()
