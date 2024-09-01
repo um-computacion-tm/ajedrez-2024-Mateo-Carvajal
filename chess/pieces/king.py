6a7251d4088f94fb3b411c1f908b628d26e78f19
Mateo-Carvajal <me.carvajal@alumno.um.edu.ar>
2024-08-31 23:29:50 -0300

    Agregando movimiento vertical y horizontal en king y subiendo test de king

--- chess/pieces/king.py
+++ chess/pieces/king.py
@@ -1,15 +1,47 @@
 from .pieces import Piece
 
 class King(Piece):
-    def __str__(self):
-        return '♚' if self.__color__ == "White" else '♔'
+    def __init__(self, color, board):
+        super().__init__(color, board)
+        self.color = color
+    white_str = "♚"
+    black_str = "♔"
 
-    def correct_movement(self, from_row, from_col, to_row, to_col):
-        mov_y = abs(from_row - from_col)
-        mov_x = abs(from_col - to_col)
+    # def correct_movement(self, from_row, from_col, to_row, to_col):
+    #     mov_y = abs(from_row - from_col)
+    #     mov_x = abs(from_col - to_col)
 
-        if mov_y > 1 or mov_x > 1:
-            return False
+    #     if mov_y > 1 or mov_x > 1:
+    #         return False
 
-
-
+    def possible_positions_vertical(self, row, col):
+        possibles = []
+        # Comprueba la fila de arriba
+        if row + 1 < 8:  # # Se asegura que se este dentro de los limites del tablero
+            other_piece = self.__board__.get_piece(row + 1, col)
+            if other_piece is None or other_piece.__color__ != self.__color__:
+                possibles.append((row + 1, col))
+        
+        # Comprueba la fila de abajo
+        if row - 1 >= 0:  # Se asegura que se este dentro de los limites del tablero
+            other_piece = self.__board__.get_piece(row - 1, col)
+            if other_piece is None or other_piece.__color__ != self.__color__:
+                possibles.append((row - 1, col))
+        
+        return possibles    
+    
+    def possible_positions_horizontal(self, row, col):
+        possibles = []
+        # Comprueba la columna a la derecha
+        if col + 1 < 8:
+            other_piece = self.__board__.get_piece(row, col + 1)
+            if other_piece is None or other_piece.__color__ != self.__color__:
+                possibles.append((row, col + 1))
+        
+        # Comprueba la columna a la izquierda
+        if col - 1 >= 0: 
+            other_piece = self.__board__.get_piece(row, col - 1)
+            if other_piece is None or other_piece.__color__ != self.__color__:
+                possibles.append((row, col - 1))
+        
+        return possibles
---
+++ tests/test_king.py
@@ -0,0 +1,35 @@
+import unittest
+from chess.pieces.king import King
+from chess.board import Board
+from chess.pieces.pawn import Pawn
+
+class TestRook(unittest.TestCase):
+
+    def test_str(self):
+        board = Board()
+        king = King("WHITE", board)
+        self.assertEqual(
+            str(king),
+            "♚",
+        )
+
+    def test_move_vertical(self):
+        board = Board()
+        king = King("WHITE", board)
+        possibles = king.possible_positions_vertical(2, 3)
+        self.assertEqual(
+            possibles,
+            [(3, 3), (1, 3)]
+        )
+
+    def test_move_horizontal(self):
+        board = Board()
+        king = King("WHITE", board)
+        possibles = king.possible_positions_horizontal(2, 3)
+        self.assertEqual(
+            possibles,
+            [(2, 4), (2, 2)]
+        )
+
+if __name__ == "__main__":
+    unittest.main()
